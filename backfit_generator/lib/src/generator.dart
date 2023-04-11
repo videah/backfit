1f23b0c0b09b59b4209884c9c6aaf9d6d9b95f6d
videah <videah@selfish.systems>
2023-04-11 12:34:24 +0100

    scratch that, revert to old working version

--- backfit_generator/lib/src/generator.dart
+++ backfit_generator/lib/src/generator.dart
@@ -399,7 +399,7 @@ return ${_typeArgs.elementAt(1).name}(
 Iterable<Parameter> _parseRequiredParameters(
         List<ParameterElement> parameters) =>
     parameters
-        .where((parameter) => parameter.isRequired)
+        .where((parameter) => parameter.isNotOptional)
         .map(_parseParameter);
 
 Iterable<Parameter> _parseOptionalParameters(
